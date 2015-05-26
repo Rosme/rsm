3d604c6114866422b292bec5767401c3fc009dbd
unknown <jsfauteux@jsfauteux-pc.pacifica.ca>
2015-05-26 16:45:21 -0400

    Changed the license and added readme

--- include/RSM/non_copyable.hpp
+++ include/RSM/non_copyable.hpp
@@ -1,8 +1,23 @@
 /*
-* Copyright (C) - Jean-Sébastien Fauteux 2015
-* This work is licensed under the Creative Commons Attribution 4.0 International License.
-* To view a copy of this license, visit http://creativecommons.org/licenses/by/4.0/
-* or send a letter to Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
+* Copyright (c) 2015 Jean-Sébastien Fauteux
+*
+* This software is provided 'as-is', without any express or implied warranty. 
+* In no event will the authors be held liable for any damages arising from 
+* the use of this software.
+*
+* Permission is granted to anyone to use this software for any purpose, 
+* including commercial applications, and to alter it and redistribute it freely, 
+* subject to the following restrictions:
+*
+* 1. The origin of this software must not be misrepresented; you must not claim 
+*    that you wrote the original software. If you use this software in a product, 
+*    an acknowledgment in the product documentation would be appreciated but is
+*    not required.
+*
+* 2. Altered source versions must be plainly marked as such, and must not be 
+*    misrepresented as being the original software.
+*
+* 3. This notice may not be removed or altered from any source distribution.
 */
 
 #pragma once
---
+++ readme.md
@@ -0,0 +1,9 @@
+# rsm_utils
+
+rsm_utils is a utility library that contains different utility for different purposes. It`s basically different tools I tend to reprogram with every object that I decided to centralise in a library. It is programmed in C++ and requires C++11.
+
+### License
+
+The library is distributed under the zlig/png license. This basically means you can use rsm_utils in any project(commercial or not, proprietary or open-source) for free. There is no restriction to the use. You don't even need to mention rsm_utils or me, though it would be appreciated.
+
+Written by Jean-Sébastien Fauteux
