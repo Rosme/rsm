650f13187ecb9948450d20198980cf1d9a2d9659
Jean-Sébastien Fauteux <jeanseb.fauteux@gmail.com>
2016-11-24 18:41:12 -0500

    Update copyright

--- test/main.cpp
+++ test/main.cpp
@@ -1,5 +1,5 @@
 /*
-* Copyright (c) 2015 Jean-Sébastien Fauteux
+* Copyright (c) 2016 Jean-SŽbastien Fauteux
 *
 * This software is provided 'as-is', without any express or implied warranty.
 * In no event will the authors be held liable for any damages arising from
